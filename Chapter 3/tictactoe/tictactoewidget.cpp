01d09182ba57d34749a7b0fadfef61b45ab47428
Alexander Hartson <alexander-hartson@live.com>
2018-02-15 22:22:48 -0600

    code change

--- Chapter 3/tictactoe/tictactoewidget.cpp
+++ Chapter 3/tictactoe/tictactoewidget.cpp
@@ -18,7 +18,7 @@ void TicTacToeWidget::initNewGame()
     }
 }
 
-TicTacToeWidget::checkWinCondition(int row, int column) const
+TicTacToeWidget::Player TicTacToeWidget::checkWinCondition(int row, int column) const
 {
     QString current = board.at(row * 3 + column)->text();
     int count = 0;
