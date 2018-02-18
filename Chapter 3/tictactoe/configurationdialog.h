ad16892f95bee86d01265ca14497337b6ba1a71a
Alexander Hartson <alexander-hartson@live.com>
2018-02-17 21:40:43 -0600

    bug fix

--- Chapter 3/tictactoe/configurationdialog.h
+++ Chapter 3/tictactoe/configurationdialog.h
@@ -10,6 +10,8 @@ class ConfigurationDialog;
 class ConfigurationDialog : public QDialog
 {
     Q_OBJECT
+    Q_PROPERTY(QString player1Name READ player1Name WRITE setPlayer1Name)
+    Q_PROPERTY(QString player2Name READ player2Name WRITE setPLayer2Name)
 
 public:
     explicit ConfigurationDialog(QWidget *parent = 0);
@@ -21,7 +23,7 @@ public:
 public slots:
     void updateOKButtonState();
     void setPlayer1Name(const QString &p1name);
-    void setPlayer2Name(const QString &p2name)
+    void setPlayer2Name(const QString &p2name);
 
 protected:
     void changeEvent(QEvent *e);
