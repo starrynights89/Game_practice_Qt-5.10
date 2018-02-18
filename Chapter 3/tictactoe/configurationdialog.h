f476d02af1d2fdbff6a2e3817618302254dad6d7
Alexander Hartson <alexander-hartson@live.com>
2018-02-17 23:37:31 -0600

    adding code

--- Chapter 3/tictactoe/configurationdialog.h
+++ Chapter 3/tictactoe/configurationdialog.h
@@ -11,7 +11,7 @@ class ConfigurationDialog : public QDialog
 {
     Q_OBJECT
     Q_PROPERTY(QString player1Name READ player1Name WRITE setPlayer1Name)
-    Q_PROPERTY(QString player2Name READ player2Name WRITE setPLayer2Name)
+    Q_PROPERTY(QString player2Name READ player2Name WRITE setPlayer2Name)
 
 public:
     explicit ConfigurationDialog(QWidget *parent = 0);
