0357e1f7d89481ae7a0ef0976197d371be5bec5f
Alexander Hartson <alexander-hartson@live.com>
2018-02-17 20:21:59 -0600

    code change

---
+++ Chapter 3/tictactoe/configurationdialog.cpp
@@ -0,0 +1,27 @@
+#include "configurationdialog.h"
+#include "ui_configurationdialog.h"
+
+ConfigurationDialog::ConfigurationDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ConfigurationDialog)
+{
+    ui->setupUi(this);
+}
+
+ConfigurationDialog::~ConfigurationDialog()
+{
+    delete ui;
+}
+
+void ConfigurationDialog::changeEvent(QEvent *e)
+{
+    QDialog::changeEvent(e);
+    switch (e->type())
+    {
+        case QEvent::LanguageChange:
+            ui->retranslateUi(this);
+            break;
+        default:
+            break;
+    }
+}
