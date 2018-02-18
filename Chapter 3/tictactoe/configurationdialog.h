5b84ba720ec6ae4322167f2101fc04eebe3b4d76
Alexander Hartson <alexander-hartson@live.com>
2018-02-17 21:26:40 -0600

    adding code

--- Chapter 3/tictactoe/configurationdialog.h
+++ Chapter 3/tictactoe/configurationdialog.h
@@ -15,6 +15,9 @@ public:
     explicit ConfigurationDialog(QWidget *parent = 0);
     ~ConfigurationDialog();
 
+    QString player1Name() const;
+    QString player2Name() const;
+
 public slots:
     void updateOKButtonState();
     void setPlayer1Name(const QString &p1name);
