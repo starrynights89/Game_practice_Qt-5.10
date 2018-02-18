52ccf272b789dc1fed6df8b00e573a928e36c87d
Alexander Hartson <alexander-hartson@live.com>
2018-02-17 23:52:48 -0600

    bug fix

--- Chapter 3/build-tictactoe-Desktop_Qt_5_10_0_MinGW_32bit-Debug/ui_configurationdialog.h
+++ Chapter 3/build-tictactoe-Desktop_Qt_5_10_0_MinGW_32bit-Debug/ui_configurationdialog.h
@@ -22,7 +22,7 @@
 
 QT_BEGIN_NAMESPACE
 
-class Ui_Dialog
+class Ui_ConfigurationDialog
 {
 public:
     QGridLayout *gridLayout;
@@ -32,34 +32,34 @@ public:
     QLineEdit *lineEdit;
     QDialogButtonBox *buttonBox;
 
-    void setupUi(QDialog *Dialog)
+    void setupUi(QDialog *ConfigurationDialog)
     {
-        if (Dialog->objectName().isEmpty())
-            Dialog->setObjectName(QStringLiteral("Dialog"));
-        Dialog->resize(254, 108);
-        gridLayout = new QGridLayout(Dialog);
+        if (ConfigurationDialog->objectName().isEmpty())
+            ConfigurationDialog->setObjectName(QStringLiteral("ConfigurationDialog"));
+        ConfigurationDialog->resize(254, 108);
+        gridLayout = new QGridLayout(ConfigurationDialog);
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
-        player1Name = new QLabel(Dialog);
+        player1Name = new QLabel(ConfigurationDialog);
         player1Name->setObjectName(QStringLiteral("player1Name"));
 
         gridLayout->addWidget(player1Name, 0, 0, 1, 1);
 
-        lineEdit_2 = new QLineEdit(Dialog);
+        lineEdit_2 = new QLineEdit(ConfigurationDialog);
         lineEdit_2->setObjectName(QStringLiteral("lineEdit_2"));
 
         gridLayout->addWidget(lineEdit_2, 1, 1, 1, 1);
 
-        player2Name = new QLabel(Dialog);
+        player2Name = new QLabel(ConfigurationDialog);
         player2Name->setObjectName(QStringLiteral("player2Name"));
 
         gridLayout->addWidget(player2Name, 1, 0, 1, 1);
 
-        lineEdit = new QLineEdit(Dialog);
+        lineEdit = new QLineEdit(ConfigurationDialog);
         lineEdit->setObjectName(QStringLiteral("lineEdit"));
 
         gridLayout->addWidget(lineEdit, 0, 1, 1, 1);
 
-        buttonBox = new QDialogButtonBox(Dialog);
+        buttonBox = new QDialogButtonBox(ConfigurationDialog);
         buttonBox->setObjectName(QStringLiteral("buttonBox"));
         buttonBox->setOrientation(Qt::Horizontal);
         buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
@@ -72,26 +72,26 @@ public:
 #endif // QT_NO_SHORTCUT
         QWidget::setTabOrder(lineEdit, lineEdit_2);
 
-        retranslateUi(Dialog);
-        QObject::connect(buttonBox, SIGNAL(rejected()), Dialog, SLOT(reject()));
-        QObject::connect(buttonBox, SIGNAL(accepted()), Dialog, SLOT(accept()));
-        QObject::connect(lineEdit, SIGNAL(textChanged(QString)), Dialog, SLOT(updateOKButtonState()));
-        QObject::connect(lineEdit_2, SIGNAL(textChanged(QString)), Dialog, SLOT(updateOKButtonState()));
+        retranslateUi(ConfigurationDialog);
+        QObject::connect(buttonBox, SIGNAL(rejected()), ConfigurationDialog, SLOT(reject()));
+        QObject::connect(buttonBox, SIGNAL(accepted()), ConfigurationDialog, SLOT(accept()));
+        QObject::connect(lineEdit, SIGNAL(textChanged(QString)), ConfigurationDialog, SLOT(updateOKButtonState()));
+        QObject::connect(lineEdit_2, SIGNAL(textChanged(QString)), ConfigurationDialog, SLOT(updateOKButtonState()));
 
-        QMetaObject::connectSlotsByName(Dialog);
+        QMetaObject::connectSlotsByName(ConfigurationDialog);
     } // setupUi
 
-    void retranslateUi(QDialog *Dialog)
+    void retranslateUi(QDialog *ConfigurationDialog)
     {
-        Dialog->setWindowTitle(QApplication::translate("Dialog", "Dialog", nullptr));
-        player1Name->setText(QApplication::translate("Dialog", "Player &1 Name:", nullptr));
-        player2Name->setText(QApplication::translate("Dialog", "Player &2 Name:", nullptr));
+        ConfigurationDialog->setWindowTitle(QApplication::translate("ConfigurationDialog", "Dialog", nullptr));
+        player1Name->setText(QApplication::translate("ConfigurationDialog", "Player &1 Name:", nullptr));
+        player2Name->setText(QApplication::translate("ConfigurationDialog", "Player &2 Name:", nullptr));
     } // retranslateUi
 
 };
 
 namespace Ui {
-    class Dialog: public Ui_Dialog {};
+    class ConfigurationDialog: public Ui_ConfigurationDialog {};
 } // namespace Ui
 
 QT_END_NAMESPACE
