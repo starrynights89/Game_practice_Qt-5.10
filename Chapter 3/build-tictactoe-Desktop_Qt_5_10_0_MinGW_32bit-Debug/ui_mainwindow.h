c1e6491b3c40dbc5dba8ea3d86a83c2958d31362
Alexander Hartson <alexander-hartson@live.com>
2018-02-19 15:34:24 -0600

    code changes

---
+++ Chapter 3/build-tictactoe-Desktop_Qt_5_10_0_MinGW_32bit-Debug/ui_mainwindow.h
@@ -0,0 +1,136 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.10.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+#include "tictactoewidget.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionNewGame;
+    QAction *actionQuit;
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QLabel *player1;
+    QLabel *player2;
+    TicTacToeWidget *gameBoard;
+    QMenuBar *menubar;
+    QMenu *menu_File;
+    QStatusBar *statusbar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(789, 585);
+        actionNewGame = new QAction(MainWindow);
+        actionNewGame->setObjectName(QStringLiteral("actionNewGame"));
+        QIcon icon;
+        icon.addFile(QStringLiteral("newgame.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionNewGame->setIcon(icon);
+        actionQuit = new QAction(MainWindow);
+        actionQuit->setObjectName(QStringLiteral("actionQuit"));
+        QIcon icon1;
+        icon1.addFile(QStringLiteral("application-exit.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionQuit->setIcon(icon1);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QStringLiteral("centralwidget"));
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        player1 = new QLabel(centralwidget);
+        player1->setObjectName(QStringLiteral("player1"));
+
+        verticalLayout->addWidget(player1);
+
+        player2 = new QLabel(centralwidget);
+        player2->setObjectName(QStringLiteral("player2"));
+
+        verticalLayout->addWidget(player2);
+
+        gameBoard = new TicTacToeWidget(centralwidget);
+        gameBoard->setObjectName(QStringLiteral("gameBoard"));
+
+        verticalLayout->addWidget(gameBoard);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QStringLiteral("menubar"));
+        menubar->setGeometry(QRect(0, 0, 789, 21));
+        menu_File = new QMenu(menubar);
+        menu_File->setObjectName(QStringLiteral("menu_File"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QStringLiteral("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName(QStringLiteral("toolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        menubar->addAction(menu_File->menuAction());
+        menu_File->addAction(actionNewGame);
+        menu_File->addSeparator();
+        menu_File->addAction(actionQuit);
+        toolBar->addAction(actionNewGame);
+        toolBar->addSeparator();
+        toolBar->addAction(actionQuit);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionNewGame->setText(QApplication::translate("MainWindow", "NewGame", nullptr));
+#ifndef QT_NO_TOOLTIP
+        actionNewGame->setToolTip(QApplication::translate("MainWindow", "Start new game", nullptr));
+#endif // QT_NO_TOOLTIP
+#ifndef QT_NO_SHORTCUT
+        actionNewGame->setShortcut(QApplication::translate("MainWindow", "Ctrl+N", nullptr));
+#endif // QT_NO_SHORTCUT
+        actionQuit->setText(QApplication::translate("MainWindow", "Quit", nullptr));
+#ifndef QT_NO_TOOLTIP
+        actionQuit->setToolTip(QApplication::translate("MainWindow", "Exit the program", nullptr));
+#endif // QT_NO_TOOLTIP
+#ifndef QT_NO_SHORTCUT
+        actionQuit->setShortcut(QApplication::translate("MainWindow", "Ctrl+Q", nullptr));
+#endif // QT_NO_SHORTCUT
+        player1->setText(QString());
+        player2->setText(QString());
+        menu_File->setTitle(QApplication::translate("MainWindow", "&File", nullptr));
+        toolBar->setWindowTitle(QApplication::translate("MainWindow", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
